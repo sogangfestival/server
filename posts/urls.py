7526a4227c23088bf7a237ff627c5bfab5ab9a5d
Gunhot <lkh0107@sogang.ac.kr>
2023-09-04 19:19:19 +0900

    post filter && CRUD(#10)

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('', PostList.as_view()),
-    # path('<int:pk>/', TripDetail.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
 ]
