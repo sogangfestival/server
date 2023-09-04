431500f5cae053a0b2db4efc818a7ac0dbb50dc1
Gunhot <lkh0107@sogang.ac.kr>
2023-09-04 22:51:03 +0900

    filter 기능 수정 필요(#14)

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,11 @@
-from rest_framework import serializers
+from rest_framework import serializers, fields
 from .models import *
+from .models import PLACES_CHOICES, TYPE_CHOICES, COLOR_CHOICES
 
 class PostSerializer(serializers.ModelSerializer):
+    place = serializers.MultipleChoiceField(choices=PLACES_CHOICES)
+    type = serializers.MultipleChoiceField(choices=TYPE_CHOICES)
+    color = serializers.MultipleChoiceField(choices=COLOR_CHOICES)
     class Meta:
         model = Post
         fields = '__all__'
