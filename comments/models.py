48763b4789e4038e9068d85d885c1ca651d85276
woochan <wchan409@naver.com>
2023-09-04 17:38:30 +0900

    #9 comments 초안

--- comments/models.py
+++ comments/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from posts.models import Post
-from multiselectfield import MultiSelectField
+# from multiselectfield import MultiSelectField
 # Create your models here.
 class Comment(models.Model):
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
         related_name='comment_post',
-        null=True
     )
-    parent_comment = models.IntegerField(null=False, default=0)
+    parent_comment = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='cocomments',
+        null=True,
+        blank=True,
+    )
     content = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)    
+    writer = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.post.title
--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,30 @@
 from rest_framework import generics
 from .models import *
 from .serializers import *
+from datetime import datetime
 
 # Create your views here.
 class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def create_comment(self, serializer):
+        post_id = self.request.data.get('post_id')
+        password = self.request.data.get('password')
+        content = self.request.data.get('content')
+
+        post = Post.objects.get(pk=post_id)
+        
+        #작성자 이름 만들어주기
+        if post.password == password:
+            # serializer.validated_data['writer'] = '작성자'
+            writer = '작성자'
+        else:
+            comments_count = Comment.objects.filter(post=post).exclude(writer='작성자').count() + 1
+            # serializer.validated_data['writer'] = f'알로스{comments_count}'
+            writer = f'알로스{comments_count}'
+
+        #대댓글이라면 대댓글의 parent_comment_id에 댓글 id 저장
+        parent_comment_id = self.request.data.get('parent_comment')
+
+        serializer.save(post=post, writer=writer,parent_comment=parent_comment_id, content=content, created_at=datetime.now())
