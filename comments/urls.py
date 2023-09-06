ef44e772811060bccddbdefe53fd0f26b7491849
woochan <wchan409@naver.com>
2023-09-06 10:20:29 +0900

    Comments 재수정!

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 urlpatterns = [
-    # path('', PostList.as_view()),
+    path('', CommentList.as_view()),
+    path('<int:pk>/', CommentDetail.as_view()),
     # path('<int:pk>/', TripDetail.as_view()),
 ]
