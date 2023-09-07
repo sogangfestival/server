d9bbe9903e6f2f887d44297519746f5864e26869
woochan <wchan409@naver.com>
2023-09-07 11:08:21 +0900

    Comments 작성자 띄우기

--- comments/views.py
+++ comments/views.py
@@ -11,19 +11,9 @@ class CommentList(generics.ListCreateAPIView):
 
     def create_comment(self, serializer):
         post_id = self.request.data.get('post_id')
-        password = self.request.data.get('password')
+        password = int(self.request.data.get('password'))
         content = self.request.data.get('content')
-
         post = Post.objects.get(pk=post_id)
-        
-        #작성자 이름 만들어주기
-        if post.password == password:
-            # serializer.validated_data['writer'] = '작성자'
-            writer = '작성자'
-        else:
-            comments_count = Comment.objects.filter(post=post).exclude(writer='작성자').count() + 1
-            # serializer.validated_data['writer'] = f'알로스{comments_count}'
-            writer = f'알로스{comments_count}'
 
         #대댓글이라면 대댓글의 parent_comment_id에 댓글 id 저장
         parent_comment_id = self.request.data.get('parent_comment')
@@ -32,7 +22,7 @@ class CommentList(generics.ListCreateAPIView):
             parent_comment = Comment.objects.get(pk=parent_comment_id)
         else:
             parent_comment = None
-        serializer.save(post=post, writer=writer, parent_comment=parent_comment, content=content, created_at=datetime.now())
+        serializer.save(post=post, password=password, parent_comment=parent_comment, content=content, created_at=datetime.now())
         return Response(serializer.data, status = status.HTTP_201_CREATED)
     
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
