452a23487c45a7e527aa7127c4115c6ec1f9297b
woochan <wchan409@naver.com>
2023-09-13 23:49:30 +0900

    posts serializers 수정

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,11 +12,15 @@ class PostSerializer(serializers.ModelSerializer):
     type = serializers.MultipleChoiceField(choices=TYPE_CHOICES)
     color = serializers.MultipleChoiceField(choices=COLOR_CHOICES)
     created_at = serializers.SerializerMethodField() 
+    comment_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
-
+    
+    def get_comment_count(self, obj):
+        return obj.comment_post.count()
+    
     def get_created_at(self, obj):
         now = datetime.now()
         obj_created_at = datetime.combine(obj.created_at, datetime.min.time())
